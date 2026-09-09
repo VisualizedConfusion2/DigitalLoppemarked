3e8ce899da8a3c5662fb2feadca4ef6167847e92
Theodor Hennecke <thh005@edu.zealand.dk>
2026-09-09 13:06:01 +0200

    startup project

---
+++ MuligeGOfStyle/Display.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuligeGOfStyle
+{
+    public class Display : IObserver
+    {
+        public void Update(float temperature)
+        {
+            Console.WriteLine($"[Display] Viser: Aktuel temperatur er {temperature}°C");
+        }
+    }
+
+}
---
+++ MuligeGOfStyle/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuligeGOfStyle
+{
+    public class Logger : IObserver
+    {
+        public void Update(float temperature)
+        {
+            Console.WriteLine($"[Logger] Log: Temperatur ændret til {temperature}°C ({DateTime.Now:HH:mm:ss})");
+        }
+    }
+
+}
